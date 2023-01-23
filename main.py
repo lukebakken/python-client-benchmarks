d45dbeb9aae1125ca684f9e1ac701e2718a739b2
Luke Bakken <luke@bakken.io>
2023-01-23 14:46:26 -0800

    Pika producer

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-import optparse
+# -*- coding: utf-8 -*-
+import argparse
+
+# import logging
 import time
 import random
 import uuid
 
+# LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) -35s %(lineno) -5d: %(message)s')
+# LOGGER = logging.getLogger(__name__)
+# logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
 
-def pika_setup():
+
+def pika_setup(host, port):
     import pika
 
-    conn = pika.BlockingConnection(pika.ConnectionParameters())
+    param = pika.ConnectionParameters(host=host, port=port)
+    conn = pika.BlockingConnection(param)
     channel = conn.channel()
     qname = str(uuid.uuid4())
     channel.queue_declare(queue=qname, auto_delete=True, exclusive=True)
@@ -22,28 +30,43 @@ def pika_publish(args, body, count):
     conn.close()
 
 
-parser = optparse.OptionParser()
-parser.add_option(
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument(
+    "-h",
+    "--host",
+    dest="host",
+    default="shostakovich",
+    help="RabbitMQ host name",
+)
+parser.add_argument(
+    "-p",
+    "--port",
+    dest="port",
+    default=5672,
+    type=int,
+    help="RabbitMQ port",
+)
+parser.add_argument(
     "-C",
     "--pmessages",
     dest="msgcount",
-    default=10000,
-    type="int",
+    default=100000,
+    type=int,
     help="producer message count",
 )
-parser.add_option(
+parser.add_argument(
     "-s",
     "--size",
     dest="msgsize",
     default=1024,
-    type="int",
+    type=int,
     help="message size in bytes",
 )
-(options, args) = parser.parse_args()
+args = parser.parse_args()
 
-body = random.randbytes(options.msgsize)
+body = random.randbytes(args.msgsize)
 
-args = pika_setup()
+pika_data = pika_setup(args.host, args.port)
 print("start: %s" % (time.ctime(time.time())))
-pika_publish(args, body, options.msgcount)
+pika_publish(pika_data, body, args.msgcount)
 print("end: %s" % (time.ctime(time.time())))
